fc0476cbc1b42212a54c4a26895552041e2b57a4
Rohit <rohitrrajkumar55@gmail.com>
2026-02-13 23:45:27 +0530

    add steps to login and verify

--- tests/navigate.spec.ts
+++ tests/navigate.spec.ts
@@ -3,6 +3,14 @@ import { getEnv } from './utils/env';
 
 test('homepage loads', async ({ page }) => {
   const url = getEnv("URL");
+  const username = getEnv("USERNAME");
+  const password = getEnv("PASSWORD");
+
   await page.goto(url);
+  expect(page.getByRole("heading",{name:"Login"})).toBeVisible();
+  await page.getByTestId("name").fill(username);
+  await page.getByTestId("password").fill(password);
+  await page.getByTestId("submit").click();
+  expect(page.getByRole("heading",{name:"Mein Sky auf einen Blick"})).toBeVisible();
   console.log("URL: "+ url);
 });
